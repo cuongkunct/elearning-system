d97ff377dd711ac41ef58addd969bc38272994df
Cuong Dev <cuongkun.ct@gmail.com>
2026-01-21 20:47:41 +0700

    Fix bug

--- app/(user)/courses/_components/CourseCardItem.tsx
+++ app/(user)/courses/_components/CourseCardItem.tsx
@@ -3,14 +3,26 @@
 import { useState } from "react";
 import { Card, CardBody, CardFooter } from "@heroui/react";
 import { Course } from "@/types/user/course/course.type";
-import { cancelCourseByMaKhoaHoc, joinCourseByMaKhoaHoc } from "@/services/user/courses/course.service";
+import {
+  cancelCourseByMaKhoaHoc,
+  joinCourseByMaKhoaHoc,
+} from "@/services/user/courses/course.service";
 import NotificationModal from "@/components/user/shared/NotificationModal";
 import ButtonJoinCourse from "./BottonJoinCourse";
 import Link from "next/link";
 import SafeImage from "./SafeImage";
 
-
-export default function CourseCardItem({ course, isJoined }: { course: Course, isJoined: boolean }) {
+export default function CourseCardItem({
+  course,
+  isJoined,
+  onJoinSuccess,
+  onCancelSuccess,
+}: {
+  course: Course;
+  isJoined: boolean;
+  onJoinSuccess: (course: Course) => void;
+  onCancelSuccess: (maKhoaHoc: string) => void;
+}) {
   const [open, setOpen] = useState(false);
   const [err, setErr] = useState<string | null>(null);
   const [title, setTitle] = useState<string | null>(null);
@@ -21,6 +33,7 @@ export default function CourseCardItem({ course, isJoined }: { course: Course, i
       setLoading(true);
       const res = await joinCourseByMaKhoaHoc(course.maKhoaHoc);
       if (res === "Ghi danh thành công!") {
+        onJoinSuccess(course);
         setTitle("Join course successfully");
         setErr(null);
         setOpen(true);
@@ -37,7 +50,9 @@ export default function CourseCardItem({ course, isJoined }: { course: Course, i
     try {
       setLoading(true);
       const res = await cancelCourseByMaKhoaHoc(course.maKhoaHoc);
+
       if (res === "Hủy ghi danh thành công!") {
+        onCancelSuccess(course.maKhoaHoc);
         setTitle("Cancel course successfully");
         setErr(null);
         setOpen(true);
@@ -53,9 +68,7 @@ export default function CourseCardItem({ course, isJoined }: { course: Course, i
   return (
     <Card className="cursor-pointer w-full">
       <Link href={`/course/${course.maKhoaHoc}`}>
-        <CardBody
-          className="p-0 overflow-hidden shrink-0 flex items-center justify-center h-[180px]"
-        >
+        <CardBody className="p-0 overflow-hidden shrink-0 flex items-center justify-center h-[180px]">
           <SafeImage
             src={course.hinhAnh}
             alt={course.tenKhoaHoc}
@@ -64,10 +77,7 @@ export default function CourseCardItem({ course, isJoined }: { course: Course, i
             className="w-full h-full object-cover aspect-square rounded-md"
           />
         </CardBody>
-        <CardFooter
-          className="flex flex-col items-start gap-2 w-full"
-
-        >
+        <CardFooter className="flex flex-col items-start gap-2 w-full">
           <div className="flex flex-col gap-2 ">
             <h3 className="text-lg font-bold">{course.tenKhoaHoc}</h3>
             <p>
