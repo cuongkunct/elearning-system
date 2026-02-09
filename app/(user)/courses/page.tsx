f170af54d1a174fffc726f99bfdf85a307591d61
Cuong Dev <cuongkun.ct@gmail.com>
2026-02-09 13:05:45 +0700

    Fix error run build eslin

--- app/(user)/courses/page.tsx
+++ app/(user)/courses/page.tsx
@@ -1,10 +1,8 @@
 import { Metadata } from "next";
 
-import CategoriesFilter from "./_components/CategoriesFilter";
 import CourseListPagination from "./_components/CourseListPagination";
 import CategoriesIntro from "./_components/CategoriesIntro";
-import SortButtons from "./_components/SortButtons";
-import CourseCard from "./_components/CourseList";
+
 
 import { Course } from "@/types/user/course/course.type";
 import {
@@ -38,7 +36,6 @@ export default async function CategoriesPage({
     currentPage?: number;
     totalPages?: number;
   };
-  let showPagination = false;
 
   if (!id) {
     const res = await getCoursesPagination(currentPage);
@@ -85,42 +82,7 @@ export default async function CategoriesPage({
             <CategoriesIntro />
           </div>
         )}
-        {/* <div className="">
-          {categories?.length > 0 && (
-            <CategoriesFilter categories={categories} />
-          )}
-        </div> */}
-        {/* <div className="flex-[7]">
-          <SortButtons />
-          <div className="flex justify-end mb-2">
-            <p className="text-gray-700 text-sm font-semibold mb-2 mr-0">
-              {courseList?.items?.length
-                ? `${courseList.items.length} course${courseList.items.length > 1 ? "s" : ""} found`
-                : "No courses found"}
-            </p>
-          </div>
-          {showPagination ? (
-            <>
-              <CourseCard courses={courseList.items} />
-              <div className="flex items-center justify-center p-4">
-                <CourseListPagination
-                  currentPage={courseList.currentPage}
-                  totalPages={courseList.totalPages}
-                />
-              </div>
-            </>
-          ) : (
-            <>
-              <CourseCard courses={courseList?.items} />
-            </>
-          )}
-        </div> */}
       </div>
-      {/* {showPagination ? (
-        <div className="py-18">
-          <CategoriesIntro />
-        </div>
-      ) : null} */}
     </div>
   );
 }
